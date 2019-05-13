19dee335a3b8d3eeb9af07a32789ec0014441fb2
exlipy <41883655+exlipy@users.noreply.github.com>
2019-05-13 13:43:15 +1000

    Ass2

---
+++ Assignment2/myQuack.py
@@ -0,0 +1,153 @@
+
+'''
+
+Scaffolding code for the Machine Learning assignment. 
+
+You should complete the provided functions and add more functions and classes as necessary.
+ 
+You are strongly encourage to use functions of the numpy, sklearn and tensorflow libraries.
+
+You are welcome to use the pandas library if you know it.
+
+
+'''
+import numpy as np
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def my_team():
+    '''
+    Return the list of the team members of this assignment submission as a list
+    of triplet of the form (student_number, first_name, last_name)
+    
+    '''
+#    return [ (1234567, 'Ada', 'Lovelace'), (1234568, 'Grace', 'Hopper'), (1234569, 'Eva', 'Tardos') ]
+    return [(9776460, 'Rune' 'Leistad'), ('12345678', 'Jenny', 'Griffiths')]
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+def prepare_dataset(dataset_path):
+    '''  
+    Read a comma separated text file where 
+	- the first field is a ID number 
+	- the second field is a class label 'B' or 'M'
+	- the remaining fields are real-valued
+
+    Return two numpy arrays X and y where 
+	- X is two dimensional. X[i,:] is the ith example
+	- y is one dimensional. y[i] is the class label of X[i,:]
+          y[i] should be set to 1 for 'M', and 0 for 'B'
+
+    @param dataset_path: full path of the dataset text file
+
+    @return
+	X,y
+    '''
+    
+    file = open(dataset_path, 'r') # Opening file from dataset_path file path
+    data = []
+    diagnosis = []
+    counter = 0
+    for line in file:
+        # Appending a list split by a comma in the line to X
+        data.append(line.split(','))
+        diagnosis.append(data.remove(data[counter][1])
+        data.remove(data[counter][0])
+        
+        # If 'M' is found in the diagnosis, replace with a 1. If not ('B' found)
+        # replace with a 0. This is assuming that there are only 'B' and 'M'
+        # found as a diagnosis
+  '''      if data[counter][1] == 'M':
+            data[counter][1] = 1
+        else:
+            data[counter][1] = 0
+        counter+=1'''
+    
+    X = np.array(data)
+    y = np.array(diagnosis)
+    print(y[0])
+    print(X[0][2])
+    
+    file.close() # Closing the opened file
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_DecisionTree_classifier(X_training, y_training):
+    '''  
+    Build a Decision Tree classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_NearrestNeighbours_classifier(X_training, y_training):
+    '''  
+    Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_SupportVectorMachine_classifier(X_training, y_training):
+    '''  
+    Build a Support Vector Machine classifier based on the training set X_training, y_training.
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def build_NeuralNetwork_classifier(X_training, y_training):
+    '''  
+    Build a Neural Network with two dense hidden layers classifier 
+    based on the training set X_training, y_training.
+    Use the Keras functions from the Tensorflow library
+
+    @param 
+	X_training: X_training[i,:] is the ith example
+	y_training: y_training[i] is the class label of X_training[i,:]
+
+    @return
+	clf : the classifier built in this function
+    '''
+    ##         "INSERT YOUR CODE HERE"    
+    raise NotImplementedError()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if __name__ == "__main__":
+    # Write a main part that calls the different 
+    # functions to perform the required tasks and repeat your experiments.
+
+
+    # call your functions here
+    prepare_dataset('medical_records.data')
+    
+
+
