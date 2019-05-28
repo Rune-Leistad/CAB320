21185c35346c4f732c4b356d7e8951584408fc95
jennybg <jennybg@stud.ntnu.no>
2019-05-28 12:37:19 +1000

    fikset opp i prepare_dataset

--- Assignment2/myQuack.py
+++ Assignment2/myQuack.py
@@ -12,9 +12,12 @@ You are welcome to use the pandas library if you know it.
 
 '''
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
-#from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, mean_squared_error 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -49,31 +52,26 @@ def prepare_dataset(dataset_path):
 	X,y
     '''
     
-    file = open(dataset_path, 'r') # Opening file from dataset_path file path
-    data = []
-    diagnosis = []
-    counter = 0
-    for line in file:
-        # Appending a list split by a comma in the line to X
-        data.append(line.split(','))
-        diagnosis.append(data.remove(data[counter][1])
-        data.remove(data[counter][0])
-        
-        # If 'M' is found in the diagnosis, replace with a 1. If not ('B' found)
-        # replace with a 0. This is assuming that there are only 'B' and 'M'
-        # found as a diagnosis
-  '''      if data[counter][1] == 'M':
-            data[counter][1] = 1
+    records = open(dataset_path,'r')
+    y = []
+    X = []
+    
+    for line in records.readlines():
+        record = line.rstrip().split(',')
+        if record[1]=='M':
+            y.append(1)
         else:
-            data[counter][1] = 0
-        counter+=1'''
+            y.append(0)
+        
+        X.append(list(record[2:]))
     
-    X = np.array(data)
-    y = np.array(diagnosis)
-    print(y[0])
-    print(X[0][2])
+    X = np.array(X)
+    X = X.astype(np.float64)
     
-    file.close() # Closing the opened file
+    y = np.array(y)
+    y = y.astype(np.float64)
+    
+    return X, y
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -93,48 +91,51 @@ def build_DecisionTree_classifier(X_training, y_training):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def build_NearrestNeighbours_classifier(X_training, y_training):
+def build_NearestNeighbours_classifier(X_training, y_training,K):
     '''  
     Build a Nearrest Neighbours classifier based on the training set X_training, y_training.
 
     @param 
 	X_training: X_training[i,:] is the ith example
 	y_training: y_training[i] is the class label of X_training[i,:]
+    K: number of neighbors
 
     @return
 	clf : the classifier built in this function
     '''
     ##         "INSERT YOUR CODE HERE"   
-    K = 5
-    clf = KneighborsClassifier(n_neighbors=K,algorithm='auto').fit(X_training,y_training)
+    clf = KNeighborsClassifier(n_neighbors=K,algorithm='auto').fit(X_training,y_training)
     
-    """
-    hentet fra https://scikit-learn.org/stable/auto_examples/neighbors/plot_classification.html#sphx-glr-auto-examples-neighbors-plot-classification-py
-    aner ikke om det funker eller ikke, men hadde vært fint med noen plots
     
-    # Plot the decision boundary. For that, we will assign a color to each
-    # point in the mesh [x_min, x_max]x[y_min, y_max].
-    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-    # Put the result into a color plot
-    Z = Z.reshape(xx.shape)
-    plt.figure()
-    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
-
-    # Plot also the training points
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
-                edgecolor='k', s=20)
-    plt.xlim(xx.min(), xx.max())
-    plt.ylim(yy.min(), yy.max())
-    plt.title("3-Class classification (k = %i, weights = '%s')"
-              % (n_neighbors, weights))
-    
-    """
     return clf
+
+def test_kNN(K_values, X_train, X_test, y_train, y_test):
+    """
+    @param
+    K_values: Nearest neighbor values to be tested
+    """
+    error = []
+    min_error = math.inf
+    
+    for K in K_values:  
+        clf = build_NearestNeighbours_classifier(X_train, y_train, K)
+        pred_i = clf.predict(X_test)
+        mean_error = np.mean(pred_i != y_test)
+        if mean_error < min_error:
+            min_error = mean_error
+            best_k = K
+        error.append(mean_error)
+        #print(classification_report(y_test, y_pred)) 
+    
+    plt.figure(figsize=(12, 6))  
+    plt.plot(range(1, 40), error, color='red', linestyle='dashed', marker='o',  
+             markerfacecolor='blue', markersize=10)
+    plt.title('Error Rate K Value')  
+    plt.xlabel('K Value')  
+    plt.ylabel('Mean Error')
+    
+    return best_k
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -178,7 +179,23 @@ if __name__ == "__main__":
 
 
     # call your functions here
-    prepare_dataset('medical_records.data')
+    # prepare data sets
+    X,y = prepare_dataset('medical_records.data')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    
+    
+    # train and test nearest neighbor classifier
+    K_values = np.arange(1,40)
+    best_k = test_kNN(K_values, X_train, X_test, y_train, y_test)
+    print('One possible optimal value of K is: ' + str(best_k))
+    
+    
+    
+    
+    #for precision evaluation of algorithm
+    # https://stackabuse.com/k-nearest-neighbors-algorithm-in-python-and-scikit-learn/
+    #print(confusion_matrix(y_test, y_pred))  
+    #print(classification_report(y_test, y_pred)) 
     
 
 
